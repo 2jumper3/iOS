982fab8c99465b987c79dae5f51d55f6f3ec9f22
2jumper3 <2jumper3@gmail.com>
2018-12-07 23:06:36 +0300

    все 3 программы, не нашел команду как вводить значение с клавиатуры. Задал как переменные изначально

---
+++ 1I_Udalov Sergey/1I_Udalov Sergey/main.swift
@@ -0,0 +1,59 @@
+//
+//  main.swift
+//  1I_Udalov Sergey
+//
+//  Created by Елизавета Кубански on 07.12.2018.
+//  Copyright © 2018 Удалов Сергей. All rights reserved.
+//
+
+import Foundation
+
+//Question 1
+
+//let a: Float = 1
+//let b: Float = 2
+//let c: Float = 3
+//var d: Float = 0
+//var x: Float = 0
+//var y: Float = 0
+//
+//d = b*b-4*a*c
+//if d < 0 {
+//    print ("no results")
+//}
+//if d == 0 {
+//    x = b/(2*a) * -1
+//    print (x)
+//}
+//if d > 0 {
+//    x = (-b + sqrt(d))/(2*a)
+//    y = (-b - sqrt(d))/(2*a)
+//    print (x)
+//}
+
+//Question 2
+
+//
+//let a: Float = 2
+//let b: Float = 2
+//var c: Float = 0
+//var s: Float = 0
+//var p: Float = 0
+//
+//s = a*b/2
+//c = sqrt((a*a+b*b))
+//p = a+b+c
+//
+//print (s,c,p  )
+
+//
+//Question 3
+//
+//var sum: Double  = 0
+//var percent: Double = 8
+//var vznos: Double = 1000
+//var days: Double = 5*365
+//var n: Double = 18
+//sum = vznos*pow(1+(percent*days)/(100*365),n)
+//
+//print (sum)
